d88feed689a06bf4a9cd6169f58cbf6477e0ea0b
bvjones <benvaughanjones@gmail.com>
2017-02-02 11:59:38 +0000

    increases temperature by 1c

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -4,4 +4,14 @@ describe('Thermostat', function(){
   it('should start with a temperature of 20c', function(){
     expect(thermostat.temperature).toBe(20);
   });
+
+  describe('Increase', function(){
+    it('should increase temperature', function(){
+      expect(thermostat.increase).toBeDefined();
+    });
+    it('should increase temperature by 1c', function(){
+      thermostat.increase();
+      expect(thermostat.temperature).toBe(21);
+    });
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,7 @@
 Thermostat = function(){
   this.temperature = 20;
 };
+
+Thermostat.prototype.increase = function () {
+  this.temperature += 1;
+};
