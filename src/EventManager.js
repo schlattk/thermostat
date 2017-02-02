34dbf67b19375add868d3a6eb79cbdac9b061e95
konradschlatte <konrad.schlatte@btinternet.com>
2017-02-02 20:51:46 +0000

    first commit

--- src/EventManager.js
+++ src/EventManager.js
@@ -1,13 +1,20 @@
 $('#Up').on("click", function (){
   thermostat.increase()
+  $('#Temperature').text(thermostat.temperature);
+  $('#energyUsage').text(thermostat.energyUsage());
+
 });
 
 $('#Down').on("click", function (){
   thermostat.decrease()
+  $('#Temperature').text(thermostat.temperature);
+  $('#energyUsage').text(thermostat.energyUsage());
 });
 
 $('#Reset').on("click", function (){
   thermostat.reset()
+  $('#Temperature').text(thermostat.temperature);
+  $('#energyUsage').text(thermostat.energyUsage());
 });
 
 $('#powerStatus').change(function (){
@@ -18,3 +25,5 @@ $('#powerStatus').change(function (){
     thermostat.powerSaverOff()
   }
 });
+$('#Temperature').text(thermostat.temperature);
+$('#energyUsage').text(thermostat.energyUsage());
